7a98162b56d5e29551f7424973b30004354d8037
takuratomi <toshkurapgmail.com>
2018-03-11 09:54:02 -0500

    se adiciona un inserí de usuario sin hacer uso de sequencias

--- database/datos.sql
+++ database/datos.sql
@@ -1,7 +1,7 @@
 
-DML
+-- DML
 
--- Insert de administrador
+-- Insert de administrador con secuensias
 insert into usuario(id,
 primer_nombre,
 primer_apellido, 
@@ -9,6 +9,21 @@ segundo_apellido,
 num_identificacion, 
 tipo_identificacion, 
 rol ) values(select nextval('usuario_id_seq'),
+'alejandro',
+'kuratomi',
+'nakamura',
+1113648236,
+3,
+1);
+
+-- Insert de administrador sin sencuencias
+insert into usuario(id,
+primer_nombre,
+primer_apellido, 
+segundo_apellido, 
+num_identificacion, 
+tipo_identificacion, 
+rol ) values(1,
 'alejandro',
 'kuratomi',
 'nakamura',
