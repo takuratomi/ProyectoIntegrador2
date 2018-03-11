36e9e9622c06c1a08fc8951ec49c7990dfce1067
Vyrmion <36672079+Vyrmion@users.noreply.github.com>
2018-03-11 13:57:22 -0500

    Creacion de daos
    Falta crear dao de padre y dao de hijo.

---
+++ ServiceVentaComida/src/main/java/co/edu/usbcali/dao/IAlmuerzoDAO.java
@@ -0,0 +1,17 @@
+package co.edu.usbcali.dao;
+
+import java.util.List;
+
+import co.edu.usbcali.modelo.Almuerzo;
+
+public interface IAlmuerzoDAO {
+
+	public void crear(Almuerzo entity);
+	public void modificar(Almuerzo entity);
+	public void borrar(Almuerzo entity);
+	public Almuerzo consultarPorId(int idAlmuerzo);
+	public Almuerzo consultarPorIdentificacion(Integer idAlmuerzo);
+	public List<Almuerzo> consultarTodos();
+	
+	
+}
