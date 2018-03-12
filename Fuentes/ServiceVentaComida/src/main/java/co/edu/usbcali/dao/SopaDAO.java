654d55e3629e96502acbe54a57114890d661658c
takuratomi <toshkurapgmail.com>
2018-03-11 23:23:17 -0500

    daos sopa,proteina,principio,pedido,parámetros y pago

---
+++ Fuentes/ServiceVentaComida/src/main/java/co/edu/usbcali/dao/SopaDAO.java
@@ -0,0 +1,50 @@
+package co.edu.usbcali.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+import co.edu.usbcali.modelo.Sopa;
+
+@Repository
+@Scope("singleton")
+public class SopaDAO implements ISopaDAO{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public void crear(Sopa entity) {
+		sessionFactory.getCurrentSession().save(entity);		
+	}
+
+	@Override
+	public void modificar(Sopa entity) {
+		sessionFactory.getCurrentSession().update(entity);
+	}
+
+	@Override
+	public void borrar(Sopa entity) {
+		sessionFactory.getCurrentSession().delete(entity);
+	}
+
+	@Override
+	public Sopa consultarPorId(int usuId) {
+		return (Sopa)sessionFactory.getCurrentSession().get(Sopa.class, usuId);
+	}
+
+	@Override
+	public Sopa consultarPorIdentificacion(Integer idSopa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Sopa> consultarTodos() {
+		return sessionFactory.getCurrentSession().createCriteria(Sopa.class).list();
+	}
+
+}
