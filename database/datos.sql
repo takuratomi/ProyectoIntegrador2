69a22aa9dfa54fdea4720366331017a282dd29c5
takuratomi <toshkurapgmail.com>
2018-05-15 07:08:27 -0500

    nuevo script

--- database/datos.sql
+++ database/datos.sql
@@ -3,6 +3,12 @@
 
 -- Insert de administrador con secuensias
 insert into usuario(id,primer_nombre,primer_apellido,segundo_apellido,num_identificacion,tipo_identificacion,rol ) values(nextval('seq_usuario'),'alejandro','kuratomi','nakamura',1113648236,3,1);
+insert into usuario(id,primer_nombre,primer_apellido,segundo_apellido,num_identificacion,tipo_identificacion,rol ) values(nextval('seq_usuario'),'alejandro','kuratomi','nakamura',1012345,3,2);
+insert into usuario(id,primer_nombre,primer_apellido,segundo_apellido,num_identificacion,tipo_identificacion,rol ) values(nextval('seq_usuario'),'manuel','sanchez','rodriguez',2012345,3,2);
+
+
+INSERT INTO padre(id, id_usuario, telefono, direccion, usuario_creacion, fecha_creacion) VALUES (nextval('seq_padre'), ( select id from usuario where num_identificacion = 1012345 ) , '2751675', 'AV CAÑAS GORDAS 2012', 'TAKURATOMI', CURRENT_DATE);
+INSERT INTO padre(id, id_usuario, telefono, direccion, usuario_creacion, fecha_creacion) VALUES (nextval('seq_padre'), ( select id from usuario where num_identificacion = 2012345 ) , '900008', 'LAS MERCEDES carrera 23 - 90 ', 'TAKURATOMI', CURRENT_DATE);
 
 -- Insert de administrador sin sencuencias
 -- insert into usuario(id,primer_nombre,primer_apellido,segundo_apellido,num_identificacion,tipo_identificacion,rol ) values(1,'alejandro','kuratomi','nakamura',1113648236,3,1);
